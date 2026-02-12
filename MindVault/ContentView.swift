1bd06c1ac1123c963583e8cd64c9b0d4df05f1f0
肖彤宇 <xiaotongyu@tianyancha.com>
2026-02-12 09:30:43 +0800

    Initial Commit

---
+++ MindVault/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MindVault
+//
+//  Created by TYC on 2026/2/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MindVault/MindVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindVaultApp.swift
+//  MindVault
+//
+//  Created by TYC on 2026/2/12.
+//
+
+import SwiftUI
+
+@main
+struct MindVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
